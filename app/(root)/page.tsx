48cf0a6884883fd20a59e5f02a8c9f356f86457c
Miguel Rodriguez <miguel004526@gmail.com>
2026-06-07 22:05:34 -0700

    Update on Right Side Bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
 
-  const loggedIn = { firstName: 'Miguel' };
+  const loggedIn = { firstName: 'Miguel', lastName: 'Rodriguez', email: 'miguelrdz.dev@gmail' };
 
   return (
     <section className="home">
@@ -22,7 +23,15 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{}, {}]}
+      />
     </section>
   )
 }
